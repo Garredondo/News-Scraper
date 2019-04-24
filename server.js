e35fb02687d96401de66e09c1cd85dc69e578a68
George Arredondo <arredondo.george@gmail.com>
2019-04-24 16:02:11 -0500

    Configured server.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 // scraping tools
@@ -6,19 +7,33 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 // require all models
-// var db = require("./")
+// var db = require("./models");
 
 const PORT = 3000;
 
+// initialize Express
+const app = express();
 
-// middleware to parse request body as JSON
+// config middleware
+    // morgan logger for logging requests
+app.use(logger("dev"));
+    // middleware to parse request body as JSON
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
+    // set public as a static folder
 app.use(express.static("public"));
 
+// handlebars
+const exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout : "main" }));
+app.set("view engine", "handlebars");
 
+// connect to Mongo DB
+
+mongoose.connect("mongodb://localhost/_dbNAME", { useNewUrlParser : true});
+
+// routes
 
 
 // start the server
